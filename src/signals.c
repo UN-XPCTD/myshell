3edd83579e8bb7d195d0c9599bc91533f12baebc
Tristan Rayo <tristanrayo10@gmail.com>
2026-04-26 14:19:53 -0500

    Implement signal handling: SIGINT/SIGTSTP ignored in shell, SIGCHLD reaps background children

--- src/signals.c
+++ src/signals.c
@@ -1,7 +1,28 @@
-#include "signals.h"
+#include <stdio.h>
 #include <signal.h>
+#include <sys/wait.h>
+#include "signals.h"
+
+static void sigchld_handler(int sig) {
+    (void)sig;
+    int status;
+    pid_t pid;
+    while ((pid = waitpid(-1, &status, WNOHANG)) > 0)
+        printf("\n[done] %d\n", pid);
+}
 
 void signals_init(void) {
-    signal(SIGINT,  SIG_IGN);
-    signal(SIGTSTP, SIG_IGN);
+    struct sigaction sa_ign  = {0};
+    struct sigaction sa_chld = {0};
+
+    sa_ign.sa_handler = SIG_IGN;
+    sigemptyset(&sa_ign.sa_mask);
+
+    sa_chld.sa_handler = sigchld_handler;
+    sigemptyset(&sa_chld.sa_mask);
+    sa_chld.sa_flags = SA_RESTART;
+
+    sigaction(SIGINT,  &sa_ign,  NULL);
+    sigaction(SIGTSTP, &sa_ign,  NULL);
+    sigaction(SIGCHLD, &sa_chld, NULL);
 }
